dd0573505eec4375f78ff421e44791f66bb3d86e
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-03-04 16:53:50 +1100

    Add Authorization header to apollo client context

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 import { ApolloClient, gql, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client'
+import { setContext } from 'apollo-link-context'
+
+const authLink = setContext((_, { headers}) => {
+  const token = localStorage.getItem('usertoken')
+  return {
+    headers :{
+      ...headers,
+      authorization: token ? `bearer ${token}` : null
+    }
+  }
+})
+
+const httpLink = new HttpLink({ uri: SERVER_URL })
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: SERVER_URL
-  })
+  link: authLink.concat(httpLink)
 })
 
 ReactDOM.render(
