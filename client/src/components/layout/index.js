0ce0a056bef16bc418398ea35cd4f60402eb31a2
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-03-07 20:18:56 +1100

    Arrange layout into header, main and footer

--- client/src/components/layout/index.js
+++ client/src/components/layout/index.js
@@ -1,16 +1,21 @@
 import React from 'react'
-import { css } from '@emotion/react'
 import PropTypes from 'prop-types'
 import Nav from './Nav'
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Nav>
-        <a href="#">Login</a>
-        <a href="#">Signup</a>
-      </Nav>
-      {children}
+      <header>
+        <Nav>
+          <a href="#">Login</a>
+          <a href="#">Signup</a>
+        </Nav>
+      </header>
+      <main>
+        {children}
+      </main>
+      <footer>
+      </footer>
     </>
   )
 }
