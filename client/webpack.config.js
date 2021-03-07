6bbb15793d6e16fe29a28cf42a016bce4cbfa7c2
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-03-07 13:39:50 +1100

    Add html webpack plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 require('dotenv').config()
 
 const config = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'main.js'
+    path: path.resolve(__dirname, './dist'),
+    filename: 'main.bundle.js'
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'build'),
@@ -29,6 +30,9 @@ const config = {
   plugins: [
     new webpack.DefinePlugin({
       SERVER_URL: JSON.stringify(process.env.SERVER_URL)
+    }),
+    new HtmlWebpackPlugin({
+      template: __dirname + '/public/index.html'
     })
   ]
 }
