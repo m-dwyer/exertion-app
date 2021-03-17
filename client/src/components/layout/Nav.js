1c085c7a647a3c196b2bcd10793cf6f7e7c4729f
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-03-17 17:43:07 +1100

    Add basic responsiveness to nav

--- client/src/components/layout/Nav.js
+++ client/src/components/layout/Nav.js
@@ -25,19 +25,39 @@ const Nav = ({ children, ...props }) => {
         align-items: center;
       `}>exertion</a>
       <HamburgerIcon css={css`
-          visibility: ${visible ? 'hidden' : 'visible' }
-        `}
+        visibility: ${visible ? 'hidden' : 'visible'};
+
+        @media (min-width: 480px) {
+          visibility: hidden;
+        }
+      `}
       onClick={toggleMenu}
       />
       <nav css={css`
-        background-color: grey;
+        background-color: gray;
         position: fixed;
         z-index: 999;
         width: 40%;
         right: 0;
         top: 0;
         height: 100vh;
-        display: ${visible ? 'unset' : 'none'}
+        display: ${visible ? 'unset' : 'none'};
+
+        @media (min-width: 480px) {
+          display: flex;
+          justify-content: flex-end;
+          position: unset;
+          background: none;
+          height: auto;
+          ul {
+            display: flex;
+            margin: 0;
+
+            li {
+              padding: 0 1em;
+            }
+          }
+        }
       `}>
         <CloseMenu
           css={css`
@@ -45,6 +65,11 @@ const Nav = ({ children, ...props }) => {
             top: 0;
             right: 0;
             margin: 0.5em;
+
+            @media (min-width: 480px) {
+              display: none;
+              position: unset;
+            }
           `}
           onClick={toggleMenu}
         />
