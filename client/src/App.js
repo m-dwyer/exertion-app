989afbc2ff3674ad6d2c7f1c2330833eeba199f6
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-04-03 18:18:18 +1100

    Add protected route handling

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useEffect, useContext } from 'react'
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { css } from '@emotion/react'
 
 import Home from './components/Home'
-import Loading from './components/Loading'
+import ProtectedRoute, {
+  PRIVATE,
+  RESTRICTED,
+  PUBLIC
+} from './components/ProtectedRoute'
 import LoginForm from './components/LoginForm'
 import Logout from './components/Logout'
 import SignupForm from './components/SignupForm'
@@ -21,8 +20,6 @@ import { store } from './store'
 import { setToken } from './reducer'
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
-
   const { state, dispatch } = useContext(store)
   const { token } = state
 
@@ -31,7 +28,6 @@ const App = () => {
     if (token) {
       dispatch(setToken(token))
     }
-    setLoading(false)
   }, [])
 
   useEffect(() => {
@@ -39,6 +35,8 @@ const App = () => {
       localStorage.setItem('usertoken', token)
     }
   }, [token])
+
+  const isAuthenticated = token ? true : false
 
   return (
     <Router>
@@ -51,23 +49,37 @@ const App = () => {
           />
           <Card>
             <Switch>
-              <Route path="/login">
+              <ProtectedRoute
+                visibility={RESTRICTED}
+                isAuthenticated={isAuthenticated}
+                path="/login"
+              >
                 <LoginForm />
-              </Route>
+              </ProtectedRoute>
 
-              <Route path="/signup">
-                <SignupForm></SignupForm>
-              </Route>
+              <ProtectedRoute
+                visibility={RESTRICTED}
+                isAuthenticated={isAuthenticated}
+                path="/signup"
+              >
+                <SignupForm />
+              </ProtectedRoute>
 
-              <Route path="/logout">
+              <ProtectedRoute
+                visibility={PRIVATE}
+                isAuthenticated={isAuthenticated}
+                path="/logout"
+              >
                 <Logout />
-              </Route>
+              </ProtectedRoute>
 
-              <Route path="/">
-                <Loading loading={loading}>
-                  {token ? <Home /> : <Redirect to="/login" />}
-                </Loading>
-              </Route>
+              <ProtectedRoute
+                visibility={PRIVATE}
+                isAuthenticated={isAuthenticated}
+                path="/"
+              >
+                <Home />
+              </ProtectedRoute>
             </Switch>
           </Card>
         </Container>
