0b67c618806d2ee5e1507ef77c485a5cb4353c8e
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-04-09 20:02:23 +1000

    Add time with default to schema

--- server/models/Activity.js
+++ server/models/Activity.js
@@ -14,6 +14,10 @@ const schema = new mongoose.Schema({
   duration: {
     type: mongoose.Schema.Types.Number,
     required: true
+  },
+  time: {
+    type: mongoose.Schema.Types.Date,
+    default: Date.now()
   }
 })
 
